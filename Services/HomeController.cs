c6f4cb241b127a821c780cb783996a83203dbe3f
ekatarr <eraw1050@gmail.com>
2026-08-12 22:47:34 +0545

    updated image scan

---
+++ Services/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MedVaultAPI.Services
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
